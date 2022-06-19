8a68e7d13a268eb9156534ee40c8f8a9d9a41471
michael tan <michael.tan@ceridian.com>
2022-06-19 13:11:31 -0400

    test state computation

---
+++ Examples/Chapter15/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace Examples.Chapter15
+{
+    public class Tests
+    {
+        private static StatefulComputation<int, int> GetAndIncrement
+            = count =>
+            {
+                return (count, count + 1);
+            };
+
+        [Test]
+        public void TestState()
+        {
+            var q = from value1 in GetAndIncrement
+                    from value2 in GetAndIncrement
+                    from value3 in GetAndIncrement
+                    select value1 + value2 + value3;
+            var result = q(0).Value;
+            Assert.AreEqual(3, result);
+        }
+    }
+}
